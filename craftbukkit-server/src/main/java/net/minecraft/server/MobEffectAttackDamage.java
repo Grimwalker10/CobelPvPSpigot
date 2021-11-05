cb7fa7890e042cbc4e69e2678fa9cd3b72793253
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2021-11-05 09:35:50 -0500

    Add configurable potion effects

--- craftbukkit-server/src/main/java/net/minecraft/server/MobEffectAttackDamage.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/MobEffectAttackDamage.java
@@ -1,7 +1,5 @@
 package net.minecraft.server;
 
-import org.github.paperspigot.PaperSpigotConfig;
-
 public class MobEffectAttackDamage extends MobEffectList {
 
     protected MobEffectAttackDamage(int i, boolean flag, int j) {
@@ -9,7 +7,6 @@ public class MobEffectAttackDamage extends MobEffectList {
     }
 
     public double a(int i, AttributeModifier attributemodifier) {
-        // PaperSpigot - Configurable modifiers for strength and weakness effects
-        return this.id == MobEffectList.WEAKNESS.id ? (double) (PaperSpigotConfig.weaknessEffectModifier * (float) (i + 1)) : PaperSpigotConfig.strengthEffectModifier * (double) (i + 1);
+        return amount * (double) (i + 1);
     }
 }
