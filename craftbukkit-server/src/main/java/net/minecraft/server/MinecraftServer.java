578436886288514252a09eada0720202f56d95f3
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2021-11-05 10:47:31 -0500

    Start currentTick at 0

--- craftbukkit-server/src/main/java/net/minecraft/server/MinecraftServer.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/MinecraftServer.java
@@ -106,7 +106,7 @@ public abstract class MinecraftServer implements ICommandListener, Runnable, IMo
     public org.bukkit.command.ConsoleCommandSender console;
     public org.bukkit.command.RemoteConsoleCommandSender remoteConsole;
     public ConsoleReader reader;
-    public static int currentTick = (int) (System.currentTimeMillis() / 50);
+    public static int currentTick = 0;
     public final Thread primaryThread;
     public java.util.Queue<Runnable> processQueue = new java.util.concurrent.ConcurrentLinkedQueue<Runnable>();
     public int autosavePeriod;
