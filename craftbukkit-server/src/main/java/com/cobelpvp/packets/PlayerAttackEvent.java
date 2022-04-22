85f377c2d0736747739e4b040b8b29e4ae10ce3a
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-04-22 18:14:29 -0400

    Update PlayerAttackEvent.java

--- craftbukkit-server/src/main/java/com/cobelpvp/packets/PlayerAttackEvent.java
+++ craftbukkit-server/src/main/java/com/cobelpvp/packets/PlayerAttackEvent.java
@@ -4,20 +4,24 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
-public class PlayerAttackEvent extends PlayerEvent {
+public class PlayerAttackEvent extends PlayerEvent
+{
+    private static final HandlerList handlers;
 
-    private static final HandlerList handlers = new HandlerList();
+    static {
+        handlers = new HandlerList();
+    }
 
-    public PlayerAttackEvent(Player player) {
+    public PlayerAttackEvent(final Player player) {
         super(player);
     }
 
     @Override
     public HandlerList getHandlers() {
-        return handlers;
+        return PlayerAttackEvent.handlers;
     }
 
     public static HandlerList getHandlerList() {
-        return handlers;
+        return PlayerAttackEvent.handlers;
     }
 }
