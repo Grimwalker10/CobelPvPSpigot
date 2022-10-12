06309670347b2cd76fdad105354f8a02e10e598a
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-12 13:46:52 -0500

    Tracking command

--- craftbukkit-server/src/main/java/com/cobelpvp/commands/NoTrackCommand.java
+++ craftbukkit-server/src/main/java/com/cobelpvp/commands/NoTrackCommand.java
@@ -1,15 +1,13 @@
 package com.cobelpvp.commands;
 
+import net.minecraft.server.EntityTracker;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.CraftWorld;
 
-import net.minecraft.server.EntityTracker;
-
 public class NoTrackCommand extends Command {
-
     public NoTrackCommand(String name) {
         super(name);
         this.usageMessage = "/" + name + " <world name> <view distance>";
@@ -17,37 +15,37 @@ public class NoTrackCommand extends Command {
         this.setPermission("cobelpvp.command.notrack");
     }
 
-    @Override
     public boolean execute(CommandSender sender, String arg1, String[] args) {
-        if (!testPermission(sender)) {
+        if (!this.testPermission(sender)) {
             return true;
-        }
-
-        if (args != null && args.length == 2) {
+        } else if (args != null && args.length == 2) {
             String worldName = args[0];
             String newNTR = args[1];
-            int trackRange = -1;
+            boolean var6 = true;
+
+            int trackRange;
             try {
                 trackRange = Integer.parseInt(newNTR);
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException var10) {
                 sender.sendMessage("'" + newNTR + "' is not a valid integer.");
                 return false;
             }
+
             trackRange = Math.max(trackRange, 0);
             World world = Bukkit.getWorld(worldName);
             if (world != null) {
-                CraftWorld craftworld = (CraftWorld) world;
+                CraftWorld craftworld = (CraftWorld)world;
                 EntityTracker entityTracker = craftworld.getHandle().getTracker();
                 entityTracker.setNoTrackDistance(trackRange);
                 sender.sendMessage("Track distance of world '" + worldName + "' was set to " + trackRange);
             } else {
                 sender.sendMessage("World '" + worldName + "' was not found!");
             }
+
             return true;
         } else {
             sender.sendMessage("[Spigot] Command - notrack: " + this.description + "\nUsage: " + this.usageMessage);
+            return false;
         }
-        return false;
     }
-
 }
