126d4de3d5f8dad661a0c7e1ae8250a87c036322
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-12 13:49:19 -0500

    Fixed Entity Tracker

--- craftbukkit-server/src/main/java/net/minecraft/server/EntityTracker.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/EntityTracker.java
@@ -1,21 +1,24 @@
 package net.minecraft.server;
 
-import java.util.*;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
 import net.minecraft.optimizations.util.IndexedLinkedHashSet;
+import org.spigotmc.AsyncCatcher;
+import org.spigotmc.TrackingRange;
 
 public class EntityTracker {
+    private IndexedLinkedHashSet<EntityTrackerEntry> c = new IndexedLinkedHashSet();
 
-    // CobelPvP start
-    private IndexedLinkedHashSet<EntityTrackerEntry> c = new IndexedLinkedHashSet<EntityTrackerEntry>();
-    public IntHashMap trackedEntities = new IntHashMap(); // CraftBukkit - private -> public
+    public IntHashMap trackedEntities = new IntHashMap();
 
     private int noTrackDistance = 0;
+
+    private WorldServer worldServer;
+
+    private int e;
 
     public int getNoTrackDistance() {
         return this.noTrackDistance;
@@ -24,103 +27,85 @@ public class EntityTracker {
     public void setNoTrackDistance(int noTrackDistance) {
         this.noTrackDistance = noTrackDistance;
     }
-    // CobelPvP end
-    private int e;
-
 
     public EntityTracker(WorldServer worldserver) {
-        this.e = 128; // CobelPvP
+        this.worldServer = worldserver;
+        this.e = 128;
+    }
+
+    public WorldServer getWorldServer() {
+        return this.worldServer;
     }
 
     public void track(Entity entity) {
         if (entity instanceof EntityPlayer) {
-            this.addEntity(entity, 512, 2);
-            // CobelPvP start
-            /*
-            EntityPlayer entityplayer = (EntityPlayer) entity;
-            Iterator iterator = this.c.iterator();
-
-            while (iterator.hasNext()) {
-                EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry) iterator.next();
-
-                if (entitytrackerentry.tracker != entityplayer) {
-                    entitytrackerentry.updatePlayer(entityplayer);
-                }
-            }
-            */
-            // CobelPvP end
+            addEntity(entity, 512, 2);
         } else if (entity instanceof EntityFishingHook) {
-            this.addEntity(entity, 64, 5, true);
+            addEntity(entity, 64, 5, true);
         } else if (entity instanceof EntityArrow) {
-            this.addEntity(entity, 64, 20, false);
+            addEntity(entity, 64, 20, false);
         } else if (entity instanceof EntitySmallFireball) {
-            this.addEntity(entity, 64, 10, false);
+            addEntity(entity, 64, 10, false);
         } else if (entity instanceof EntityFireball) {
-            this.addEntity(entity, 64, 10, false);
+            addEntity(entity, 64, 10, false);
         } else if (entity instanceof EntitySnowball) {
-            this.addEntity(entity, 64, 10, true);
+            addEntity(entity, 64, 10, true);
         } else if (entity instanceof EntityEnderPearl) {
-            this.addEntity(entity, 64, 2, true);
+            addEntity(entity, 64, 2, true);
         } else if (entity instanceof EntityEnderSignal) {
-            this.addEntity(entity, 64, 4, true);
+            addEntity(entity, 64, 4, true);
         } else if (entity instanceof EntityEgg) {
-            this.addEntity(entity, 64, 10, true);
+            addEntity(entity, 64, 10, true);
         } else if (entity instanceof EntityPotion) {
-            this.addEntity(entity, 64, 10, true);
+            addEntity(entity, 64, 10, true);
         } else if (entity instanceof EntityThrownExpBottle) {
-            this.addEntity(entity, 64, 10, true);
+            addEntity(entity, 64, 10, true);
         } else if (entity instanceof EntityFireworks) {
-            this.addEntity(entity, 64, 10, true);
+            addEntity(entity, 64, 10, true);
         } else if (entity instanceof EntityItem) {
-            this.addEntity(entity, 64, 20, true);
+            addEntity(entity, 64, 20, true);
         } else if (entity instanceof EntityMinecartAbstract) {
-            this.addEntity(entity, 80, 3, true);
+            addEntity(entity, 80, 3, true);
         } else if (entity instanceof EntityBoat) {
-            this.addEntity(entity, 80, 3, true);
+            addEntity(entity, 80, 3, true);
         } else if (entity instanceof EntitySquid) {
-            this.addEntity(entity, 64, 3, true);
+            addEntity(entity, 64, 3, true);
         } else if (entity instanceof EntityWither) {
-            this.addEntity(entity, 80, 3, false);
+            addEntity(entity, 80, 3, false);
         } else if (entity instanceof EntityBat) {
-            this.addEntity(entity, 80, 3, false);
+            addEntity(entity, 80, 3, false);
         } else if (entity instanceof IAnimal) {
-            this.addEntity(entity, 80, 3, true);
+            addEntity(entity, 80, 3, true);
         } else if (entity instanceof EntityEnderDragon) {
-            this.addEntity(entity, 160, 3, true);
+            addEntity(entity, 160, 3, true);
         } else if (entity instanceof EntityTNTPrimed) {
-            this.addEntity(entity, 160, 10, true);
+            addEntity(entity, 160, 10, true);
         } else if (entity instanceof EntityFallingBlock) {
-            this.addEntity(entity, 160, 20, true);
+            addEntity(entity, 160, 20, true);
         } else if (entity instanceof EntityHanging) {
-            this.addEntity(entity, 160, Integer.MAX_VALUE, false);
+            addEntity(entity, 160, 2147483647, false);
         } else if (entity instanceof EntityExperienceOrb) {
-            this.addEntity(entity, 160, 20, true);
+            addEntity(entity, 160, 20, true);
         } else if (entity instanceof EntityEnderCrystal) {
-            this.addEntity(entity, 256, Integer.MAX_VALUE, false);
+            addEntity(entity, 256, 2147483647, false);
         }
     }
 
     public void addEntity(Entity entity, int i, int j) {
-        this.addEntity(entity, i, j, false);
+        addEntity(entity, i, j, false);
     }
 
     public void addEntity(Entity entity, int i, int j, boolean flag) {
-        org.spigotmc.AsyncCatcher.catchOp( "entity track"); // Spigot
-        i = org.spigotmc.TrackingRange.getEntityTrackingRange(entity, i); // Spigot
-        if (i > this.e) {
+        AsyncCatcher.catchOp("entity track");
+        i = TrackingRange.getEntityTrackingRange(entity, i);
+        if (i > this.e)
             i = this.e;
-        }
-
         try {
-            if (this.trackedEntities.b(entity.getId())) {
+            if (this.trackedEntities.b(entity.getId()))
                 throw new IllegalStateException("Entity is already tracked!");
-            }
-
-            EntityTrackerEntry entitytrackerentry = new EntityTrackerEntry(this, entity, i, j, flag); // CobelPvP
-
+            EntityTrackerEntry entitytrackerentry = new EntityTrackerEntry(this, entity, i, j, flag);
             this.c.add(entitytrackerentry);
             this.trackedEntities.a(entity.getId(), entitytrackerentry);
-            // entitytrackerentry.scanPlayers(this.world.players); // CobelPvP
             entitytrackerentry.addNearPlayers();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
@@ -128,44 +113,43 @@ public class EntityTracker {
     }
 
     public void untrackEntity(Entity entity) {
-        org.spigotmc.AsyncCatcher.catchOp( "entity untrack"); // Spigot
+        AsyncCatcher.catchOp("entity untrack");
         if (entity instanceof EntityPlayer) {
-            EntityPlayer entityplayer = (EntityPlayer) entity;
-            Iterator iterator = this.c.iterator();
-
+            EntityPlayer entityplayer = (EntityPlayer)entity;
+            Iterator<EntityTrackerEntry> iterator = this.c.iterator();
             while (iterator.hasNext()) {
-                EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry) iterator.next();
-
+                EntityTrackerEntry entitytrackerentry = iterator.next();
                 entitytrackerentry.a(entityplayer);
             }
         }
-
-        EntityTrackerEntry entitytrackerentry1 = (EntityTrackerEntry) this.trackedEntities.d(entity.getId());
-
+        EntityTrackerEntry entitytrackerentry1 = (EntityTrackerEntry)this.trackedEntities.d(entity.getId());
         if (entitytrackerentry1 != null) {
             this.c.remove(entitytrackerentry1);
             entitytrackerentry1.a();
         }
     }
 
-    // CobelPvP start - parallel tracking
-    private static int trackerThreads = 4; // <-- 3 non-this threads, one this
-    private static ExecutorService pool = Executors.newFixedThreadPool(trackerThreads - 1, new ThreadFactoryBuilder().setNameFormat("entity-tracker-%d").build());
+    private static int trackerThreads = 4;
+
+    private static ExecutorService pool = Executors.newFixedThreadPool(trackerThreads - 1, (new ThreadFactoryBuilder()).setNameFormat("entity-tracker-%d").build());
+
     public void updatePlayers() {
         int offset = 0;
-        final CountDownLatch latch = new CountDownLatch(trackerThreads); 
+        final CountDownLatch latch = new CountDownLatch(trackerThreads);
         for (int i = 1; i <= trackerThreads; i++) {
             final int localOffset = offset++;
             Runnable runnable = new Runnable() {
-                @Override
                 public void run() {
-                    for (int i = localOffset; i < c.size(); i += trackerThreads) {
-                        c.get(i).update();
-                    }
+                    for (int i = localOffset; i < EntityTracker.this.c.size(); i += EntityTracker.trackerThreads)
+                        ((EntityTrackerEntry)EntityTracker.this.c.get(i)).update();
                     latch.countDown();
                 }
             };
-            if (i < trackerThreads) pool.execute(runnable); else runnable.run();
+            if (i < trackerThreads) {
+                pool.execute(runnable);
+            } else {
+                runnable.run();
+            }
         }
         try {
             latch.await();
@@ -173,51 +157,24 @@ public class EntityTracker {
             e.printStackTrace();
         }
     }
-    // CobelPvP end
 
     public void a(Entity entity, Packet packet) {
-        EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry) this.trackedEntities.get(entity.getId());
-
-        if (entitytrackerentry != null) {
+        EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry)this.trackedEntities.get(entity.getId());
+        if (entitytrackerentry != null)
             entitytrackerentry.broadcast(packet);
-        }
     }
 
     public void sendPacketToEntity(Entity entity, Packet packet) {
-        EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry) this.trackedEntities.get(entity.getId());
-
-        if (entitytrackerentry != null) {
+        EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry)this.trackedEntities.get(entity.getId());
+        if (entitytrackerentry != null)
             entitytrackerentry.broadcastIncludingSelf(packet);
-        }
     }
 
     public void untrackPlayer(EntityPlayer entityplayer) {
-        Iterator iterator = this.c.iterator();
-
+        Iterator<EntityTrackerEntry> iterator = this.c.iterator();
         while (iterator.hasNext()) {
-            EntityTrackerEntry entitytrackerentry = (EntityTrackerEntry) iterator.next();
-
+            EntityTrackerEntry entitytrackerentry = iterator.next();
             entitytrackerentry.clear(entityplayer);
         }
     }
-
-    // CobelPvP start - nope
-    /*
-    public void a(EntityPlayer entityplayer, Chunk chunk) {
-        // Kohi start - Optimized EntityTracker
-        for (List<Entity> slice : chunk.entitySlices) {
-            for (Entity entity : slice) {
-                if (entity != entityplayer) {
-                    EntityTrackerEntry entry = (EntityTrackerEntry) trackedEntities.get(entity.getId());
-
-                    if (entry != null) {
-                        entry.updatePlayer(entityplayer);
-                    }
-                }
-            }
-        }
-        // Kohi end
-    }
-    */
-
 }
