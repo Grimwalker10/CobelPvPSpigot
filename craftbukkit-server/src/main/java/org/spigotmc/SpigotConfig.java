488fd20097cfe6d9a5e53b8d5f9dfc744f94fb63
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-20 17:38:31 -0500

    Fixed kb values.

--- craftbukkit-server/src/main/java/org/spigotmc/SpigotConfig.java
+++ craftbukkit-server/src/main/java/org/spigotmc/SpigotConfig.java
@@ -534,8 +534,8 @@ public class SpigotConfig
 
     public static double knockbackFriction = 1.8D;
     public static double knockbackHorizontal = 0.32D;
-    public static double knockbackVertical = 0.35D;
-    public static double knockbackVerticalLimit = 0.38D;
+    public static double knockbackVertical = 0.4D;
+    public static double knockbackVerticalLimit = 0.33D;
     public static double knockbackExtraHorizontal = 0.425D;
     public static double knockbackExtraVertical = 0.08D;
     
