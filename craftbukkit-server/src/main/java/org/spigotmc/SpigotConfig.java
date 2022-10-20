7c98d41e038b8eac38f56b42098b25fdf5ab30f0
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-20 08:25:15 -0500

    Update SpigotConfig.java

--- craftbukkit-server/src/main/java/org/spigotmc/SpigotConfig.java
+++ craftbukkit-server/src/main/java/org/spigotmc/SpigotConfig.java
@@ -535,7 +535,7 @@ public class SpigotConfig
     public static double knockbackFriction = 2.0D;
     public static double knockbackHorizontal = 0.32D;
     public static double knockbackVertical = 0.35D;
-    public static double knockbackVerticalLimit = 0.36D;
+    public static double knockbackVerticalLimit = 0.38D;
     public static double knockbackExtraHorizontal = 0.425D;
     public static double knockbackExtraVertical = 0.085D;
     
