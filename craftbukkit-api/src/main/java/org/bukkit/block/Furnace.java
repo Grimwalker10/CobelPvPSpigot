55b18e40eae2e7a2b3b3d6854f346d850c857d35
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-24 04:36:35 -0500

    Furne Multiplier Api Added

--- craftbukkit-api/src/main/java/org/bukkit/block/Furnace.java
+++ craftbukkit-api/src/main/java/org/bukkit/block/Furnace.java
@@ -8,6 +8,24 @@ import org.bukkit.inventory.FurnaceInventory;
 public interface Furnace extends BlockState, ContainerBlock {
 
     /**
+     * Gets the speed multiplier that this {@link Furnace} will cook
+     * compared to vanilla.
+     *
+     * @return the multiplier, a value between 0 and 200
+     */
+    double getCookSpeedMultiplier() throws IllegalArgumentException;
+
+    /**
+     * Sets the speed multiplier that this {@link Furnace} will cook
+     * compared to vanilla.
+     *
+     * @param multiplier the multiplier to set, a value between 0 and 200
+     * @throws IllegalArgumentException if value is less than 0
+     * @throws IllegalArgumentException if value is more than 200
+     */
+    void setCookSpeedMultiplier(double multiplier);
+
+    /**
      * Get burn time.
      *
      * @return Burn time
