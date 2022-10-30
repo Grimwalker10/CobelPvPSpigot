42ca9823c497971d2aec9a2423719d1074f7204b
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-10-30 16:17:24 -0500

    a little bit less unbreaking reduction

--- craftbukkit-server/src/main/java/net/minecraft/server/PlayerInventory.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/PlayerInventory.java
@@ -462,7 +462,7 @@ public class PlayerInventory implements IInventory {
     }
 
     public void a(float f) {
-        f /= org.spigotmc.SpigotConfig.reduceArmorDamage ? 12.5F : 5.2F; // CobelPvP
+        f /= org.spigotmc.SpigotConfig.reduceArmorDamage ? 10.5F : 5.2F; // CobelPvP
         if (f < 1.0F) {
             f = 1.0F;
         }
