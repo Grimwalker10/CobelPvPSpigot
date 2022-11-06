b2a36e10887e04d4f78a6ba5e271cc7cd891ada5
DiegoJVC <63890410+DiegoJVC@users.noreply.github.com>
2022-11-06 11:48:48 -0500

    Update EntityEnderPearl.java

--- craftbukkit-server/src/main/java/net/minecraft/server/EntityEnderPearl.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/EntityEnderPearl.java
@@ -21,11 +21,12 @@ public class EntityEnderPearl extends EntityProjectile
     private Location lastValidTeleport;
     private Item toRefundPearl;
     private EntityLiving c;
-    private Location lastValidLocation;
+    private static Set<Block> PROHIBITED_PEARL_BLOCKS;
     public static List<String> pearlAbleType;
     public static List<Material> forwardTypes;
 
     static {
+        EntityEnderPearl.PROHIBITED_PEARL_BLOCKS = Sets.newHashSet(Block.getById(85), Block.getById(107));
         EntityEnderPearl.pearlAbleType = Arrays.asList("STEP", "STAIR");
         EntityEnderPearl.forwardTypes = Collections.singletonList(Material.ENDER_PORTAL_FRAME);
     }
@@ -95,8 +96,8 @@ public class EntityEnderPearl extends EntityProjectile
                 final EntityPlayer entityplayer = (EntityPlayer)this.getShooter();
                 if (entityplayer.playerConnection.b().isConnected() && entityplayer.world == this.world) {
                     if (this.lastValidTeleport != null) {
-                        CraftPlayer player = entityplayer.getBukkitEntity();
-                        Location location = this.lastValidLocation.clone();
+                        final CraftPlayer player = entityplayer.getBukkitEntity();
+                        final Location location = this.lastValidTeleport;
                         location.setPitch(player.getLocation().getPitch());
                         location.setYaw(player.getLocation().getYaw());
                         final PlayerTeleportEvent teleEvent = new PlayerTeleportEvent(player, player.getLocation(), location, PlayerTeleportEvent.TeleportCause.ENDER_PEARL);
@@ -121,14 +122,57 @@ public class EntityEnderPearl extends EntityProjectile
         }
     }
 
-    // snHose start - antipearl glitch
+    @Override
     public void h() {
-        if (this.world.getCubes(this, this.boundingBox.grow(0.25D, 0.25D, 0.25D)).isEmpty()) {
-            this.lastValidLocation = getBukkitEntity().getLocation();
+        final EntityLiving shooter = this.getShooter();
+        if (shooter != null && !shooter.isAlive()) {
+            this.die();
         }
-        super.h();
+        else {
+            final AxisAlignedBB newBoundingBox = AxisAlignedBB.a(this.locX - 0.3, this.locY - 0.05, this.locZ - 0.3, this.locX + 0.3, this.locY + 0.5, this.locZ + 0.3);
+            if (!this.world.boundingBoxContainsMaterials(this.boundingBox.grow(0.25, 0.0, 0.25), EntityEnderPearl.PROHIBITED_PEARL_BLOCKS) && this.world.getCubes(this, newBoundingBox).isEmpty()) {
+                this.lastValidTeleport = this.getBukkitEntity().getLocation();
+            }
+            final org.bukkit.block.Block block = this.world.getWorld().getBlockAt(MathHelper.floor(this.locX), MathHelper.floor(this.locY), MathHelper.floor(this.locZ));
+            final Material typeHere = this.world.getWorld().getBlockAt(MathHelper.floor(this.locX), MathHelper.floor(this.locY), MathHelper.floor(this.locZ)).getType();
+            if (EntityEnderPearl.pearlAbleType.stream().anyMatch(it -> typeHere.name().contains(it))) {
+                this.lastValidTeleport = this.getBukkitEntity().getLocation();
+            }
+            if (shooter != null && EntityEnderPearl.forwardTypes.stream().anyMatch(it -> block.getRelative(getDirection((EntityPlayer)shooter)).getType() == it)) {
+                this.lastValidTeleport = this.getBukkitEntity().getLocation();
+            }
+            if (typeHere == Material.FENCE_GATE && ((Openable)block.getState().getData()).isOpen()) {
+                this.lastValidTeleport = this.getBukkitEntity().getLocation();
+            }
+            if (shooter != null) {
+                final org.bukkit.block.Block newTrap = block.getRelative(getDirection((EntityPlayer)shooter)).getRelative(BlockFace.DOWN);
+                if (newTrap.getType() == Material.COBBLE_WALL || newTrap.getType() == Material.FENCE) {
+                    this.lastValidTeleport = newTrap.getLocation();
+                }
+            }
+            super.h();
+        }
     }
-    // snHose end
+
+    public static BlockFace getDirection(final EntityPlayer entityPlayer) {
+        float yaw = entityPlayer.getBukkitEntity().getLocation().getYaw();
+        if (yaw < 0.0f) {
+            yaw += 360.0f;
+        }
+        if (yaw >= 315.0f || yaw < 45.0f) {
+            return BlockFace.SOUTH;
+        }
+        if (yaw < 135.0f) {
+            return BlockFace.WEST;
+        }
+        if (yaw < 225.0f) {
+            return BlockFace.NORTH;
+        }
+        if (yaw < 315.0f) {
+            return BlockFace.EAST;
+        }
+        return BlockFace.NORTH;
+    }
 
     public Item getToRefundPearl() {
         return this.toRefundPearl;
