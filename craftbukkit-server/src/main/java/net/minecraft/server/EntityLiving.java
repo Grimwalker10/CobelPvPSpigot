a337192bb72bd2cc97e8e5cd9eb7064e25c13f5e
DiegoJVC <justdiegovc@gmail.com>
2022-11-20 03:54:42 -0500

    Revert "Fixed BlockHit reduce damage ( no more damage reduced)"
    This reverts commit e9469175fbf14e6ff9fb1308beb1efde0a2b6793.

--- craftbukkit-server/src/main/java/net/minecraft/server/EntityLiving.java
+++ craftbukkit-server/src/main/java/net/minecraft/server/EntityLiving.java
@@ -1062,6 +1062,9 @@ public abstract class EntityLiving extends Entity {
                 @Override
                 public Double apply(Double f) {
                     if (human) {
+                        if (!damagesource.ignoresArmor() && ((EntityHuman) EntityLiving.this).isBlocking() && f > 0.0F) {
+                            return -(f - ((1.0F + f) * 0.5F));
+                        }
                     }
                     return -0.0;
                 }
